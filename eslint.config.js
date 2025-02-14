83e11bf1c7189330f7579d339b040145af93d4e3
InsectMk <makun_ing@126.com>
2025-02-14 10:57:33 +0800

    格式化内容

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,24 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
-import tseslint from "typescript-eslint"
-import pluginVue from "eslint-plugin-vue"
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  ...pluginVue.configs['flat/essential'],
   {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } }
+  }
+  /*{
     // 添加一个新的配置对象来设置规则
     rules: {
       // 禁止使用分号
       semi: ["error", "never"],
     },
-  },
+  },*/
 ]
