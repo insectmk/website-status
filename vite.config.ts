d87fd1861d5573c3682341ef23e7ece901d9571c
InsectMk <makun_ing@126.com>
2025-02-15 13:24:36 +0800

    增加控制台检查代码的插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// @ts-ignore 忽略ts检查
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue() //Vue
+    vue(), //Vue
+    eslintPlugin({ // 启用 ESLint 插件
+      cache: false,// 禁用缓存以实时检查
+      include: ['src/**/*.vue', 'src/**/*.js', 'src/**/*.ts'], // 指定需要检查的文件
+      exclude: ['node_modules/**/*'] // 排除不需要检查的文件
+    })
   ]
 })
