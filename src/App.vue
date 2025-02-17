118b8ec22d9362196a736534697a93b000c78bbf
InsectMk <makun_ing@126.com>
2025-02-17 20:15:49 +0800

    初始化项目结构

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-
+import WebsiteCard from './components/WebsiteCard.vue'
 </script>
 
 <template>
-
+  <div>
+    <p>头部</p>
+  </div>
+  <div>
+    <p>身体</p>
+    <website-card />
+  </div>
+  <div>
+    <p>尾部</p>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -10,16 +10,19 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(), //Vue
-    eslintPlugin({ // 启用 ESLint 插件
-      cache: false,// 禁用缓存以实时检查
+    eslintPlugin({
+      // 启用 ESLint 插件
+      cache: false, // 禁用缓存以实时检查
       include: ['src/**/*.vue', 'src/**/*.js', 'src/**/*.ts'], // 指定需要检查的文件
       exclude: ['node_modules/**/*'] // 排除不需要检查的文件
     }),
-    AutoImport({//Element-Plus自动导入
-      resolvers: [ElementPlusResolver()],
+    AutoImport({
+      //Element-Plus自动导入
+      resolvers: [ElementPlusResolver()]
     }),
-    Components({//Element-Plus自动导入
-      resolvers: [ElementPlusResolver()],
-    }),
+    Components({
+      //Element-Plus自动导入
+      resolvers: [ElementPlusResolver()]
+    })
   ]
 })
