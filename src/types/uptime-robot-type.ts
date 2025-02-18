2685a4e66d2afb5a80a93533bc2fdd8c2b239c51
InsectMk <makun_ing@126.com>
2025-02-18 16:37:30 +0800

    参数修改

--- src/types/uptime-robot-type.ts
+++ src/types/uptime-robot-type.ts
@@ -4,12 +4,12 @@
  */
 export interface UptimeRobotApiParams {
   api_key: string // API密钥
-  custom_uptime_ranges: string // 状态范围集合_表示从开始到结束，-表示分割，如 1731513600_1731600000-1731427200_1731513600
-  format: string // 如：json
-  log_types: string // 如：1-2
-  logs: number // 1
-  logs_start_date: number // 开始时间戳
-  logs_end_date: number // 结束时间戳
+  custom_uptime_ranges?: string // 状态范围集合_表示从开始到结束，-表示分割，如 1731513600_1731600000-1731427200_1731513600
+  format?: string // 如：json
+  log_types?: string // 如：1-2
+  logs?: number // 1
+  logs_start_date?: number // 开始时间戳
+  logs_end_date?: number // 结束时间戳
 }
 
 /**
